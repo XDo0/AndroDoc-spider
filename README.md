6dc8359a2e44e081cda5f52b3d59946760bc9810
XD <15102480190@163.com>
2021-03-12 00:18:56 +0800

    todo:method para

---
+++ README.md
@@ -0,0 +1,8 @@
+| Constants                        | Flag For                                                     | meanings                                                     | value |
+| -------------------------------- | :----------------------------------------------------------- | ------------------------------------------------------------ | ----- |
+| COMPONENT_ENABLED_STATE_DEFAULT  | `setApplicationEnabledSetting(java.lang.String, int, int)` and `setComponentEnabledSetting(android.content.ComponentName, int, int)` | Explicitly setting the component state to this value restores it's enabled state to whatever is set in the manifest. | 0     |
+| COMPONENT_ENABLED_STATE_DISABLED | `setApplicationEnabledSetting(java.lang.String, int, int)` and `setComponentEnabledSetting(android.content.ComponentName, int, int)` |                                                              |       |
+|                                  |                                                              |                                                              |       |
+|                                  |                                                              |                                                              |       |
+|                                  |                                                              |                                                              |       |
+
--- crawler.py
+++ crawler.py
@@ -5,8 +5,13 @@ import re
 import pandas
 import os
 
+def addtwodimdict(thedict, key_a, key_b, val):
+  if key_a in thedict:
+    thedict[key_a].update({key_b: val})
+  else:
+    thedict.update({key_a:{key_b: val}})
 
-CON={}#the dict of constants
+CON={}#the 2-D dict of constants
 METH={}#2-D dict :api name{parameters}
 # response = requests.get("https://developer.android.google.cn/reference/android/content/pm/PackageManager?hl=en#setApplicationEnabledSetting(java.lang.String,%20int,%20int)n")
 # soup=BeautifulSoup(response.content,'lxml')
@@ -17,11 +22,25 @@ for tag in all_tags:
         child_tag=tag.h3
         #判断是否为method/constant
         if child_tag['class'][0]=="api-name":#多值属性的返回类型是list:
-            api_name=child_tag.string
+            api_name=child_tag['data-text']
             if api_name.isupper():#字母全为大写
-                CON[api_name]=(tag.find_all('p')[-1].string[27])#get constant value,at index 27
-            elif api_name[0].islower():
-                METH
+                content=tag.find_all('p')
+                # get constant value,at index 27
+                addtwodimdict(CON,api_name,'value',(content[-1].string[27]))
+                # get all of descriptions
+                description=""
+                for data in iter(content[:-1]):
+                    for string in data.stripped_strings:
+                        description+=string
+                addtwodimdict(CON,api_name,'description',description)
+
+            elif api_name[0].islower(): # 第一个字母小写表示是method
+                content = tag.find_all('p')
+                description = ""
+                for data in iter(content):
+                    for string in data.stripped_strings:
+                        description += string
+                addtwodimdict(METH,api_name,'meanings',description)
 
 
 
@@ -29,7 +48,9 @@ for tag in all_tags:
 
 
 
-    except:
+
+    except Exception as e:
+        print(e)
         pass
 
 # api_tag=SoupStrainer(class_="api-name")
