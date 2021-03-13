204906991b13416f3908d2cce62617eaa1b65ba8
XD <15102480190@163.com>
2021-03-13 15:20:54 +0800

    write file done

--- crawler.py
+++ crawler.py
@@ -11,50 +11,50 @@ def addtwodimdict(thedict, key_a, key_b, val):
   else:
     thedict.update({key_a:{key_b: val}})
 
-CON={}#the 2-D dict of constants
-METH={}#2-D dict :api name{parameters}
-# response = requests.get("https://developer.android.google.cn/reference/android/content/pm/PackageManager?hl=en#setApplicationEnabledSetting(java.lang.String,%20int,%20int)n")
-# soup=BeautifulSoup(response.content,'lxml')
-soup=BeautifulSoup(open("test.html"),'lxml')
-all_tags=soup.find_all("div",attrs={"data-version-added":True})
-for tag in all_tags:
-    try:
-        child_tag=tag.h3
-        #判断是否为method/constant
-        if child_tag['class'][0]=="api-name":#多值属性的返回类型是list:
-            api_name=child_tag['data-text']
-            if api_name.isupper():#字母全为大写
-                content=tag.find_all('p')
-                # get constant value,at index 27
-                addtwodimdict(CON,api_name,'value',(content[-1].string[27]))
-                # get all of descriptions
-                description=""
-                for data in iter(content[:-1]):
-                    for string in data.stripped_strings:
-                        description+=string
-                addtwodimdict(CON,api_name,'description',description)
+def get_one_page(url):
+    CON = {}  # the 2-D dict of constants
+    METH = {}  # 2-D dict :api name{parameters}
 
-            elif api_name[0].islower(): # 第一个字母小写表示是method
-                content = tag.find_all('p')
-                description = ""
-                for data in iter(content):
-                    for string in data.stripped_strings:
-                        description += string
-                # 添加说明
-                addtwodimdict(METH,api_name,'meanings',description)
-                # 开始遍历表格
-                tables=tag.find_all('tbody')
-                for pm in tables:
-                    key=pm.tr.th.string
-                    row = ""
-                    for i in range(2,len(pm.contents)-1,2):
-                        a=pm.contents[i]
-                        para=pm.contents[i].text
-                        row+=str(para).replace('\n',' ')
-                        row+=";"
-                    addtwodimdict(METH,api_name,key,row)
+    # soup=BeautifulSoup(open("test.html"),'lxml')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    all_tags=soup.find_all("div",attrs={"data-version-added":True})
+    for tag in all_tags:
+        try:
+            child_tag=tag.h3
+            #判断是否为method/constant
+            if child_tag['class'][0]=="api-name":#多值属性的返回类型是list:
+                api_name=child_tag['data-text']
+                if api_name.isupper():#字母全为大写
+                    content=tag.find_all('p')
+                    # get constant value,at index 27
+                    addtwodimdict(CON,api_name,'value',(content[-1].string[27]))
+                    # get all of descriptions
+                    description=""
+                    for data in iter(content[:-1]):
+                        for string in data.stripped_strings:
+                            description+=string
+                    addtwodimdict(CON,api_name,'description',description)
 
-
+                elif api_name[0].islower(): # 第一个字母小写表示是method
+                    content = tag.find_all('p')
+                    description = ""
+                    for data in iter(content):
+                        for string in data.stripped_strings:
+                            description += string
+                    # 添加说明
+                    addtwodimdict(METH,api_name,'meanings',description)
+                    # 开始遍历表格
+                    tables=tag.find_all('tbody')
+                    for pm in tables:
+                        key=pm.tr.th.string
+                        row = ""
+                        for i in range(2,len(pm.contents)-1,2):
+                            a=pm.contents[i]
+                            para=pm.contents[i].text
+                            row+=str(para).replace('\n',' ')
+                            row+=";"
+                        addtwodimdict(METH,api_name,key,row)
 
 
                     # for tr in pm.tr.next_siblings:
@@ -70,19 +70,10 @@ for tag in all_tags:
 
                     # METH[api_name][pm.th.string]
 
-
-
-
-
-
-
-
-
-
-    except Exception as e:
-        print(e)
-        pass
-
+        except Exception as e:
+            print(e)
+            pass
+    return CON,METH
 # api_tag=SoupStrainer(class_="api-name")
 # # for tag in soup.find_all(api_tag):
 # #     print(tag.next_siblings)
